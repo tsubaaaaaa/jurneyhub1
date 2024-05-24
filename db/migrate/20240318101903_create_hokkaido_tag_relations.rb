6d7220822db79c8493344cd84176adac8ddf95c0
tsubaaaaaa <tsubahara.ryuki.v4@elms.hokudai.ac.jp>
2024-05-24 10:40:20 +0900

    7th

--- db/migrate/20240318101903_create_hokkaido_tag_relations.rb
+++ db/migrate/20240318101903_create_hokkaido_tag_relations.rb
@@ -2,7 +2,7 @@ class CreateHokkaidoTagRelations < ActiveRecord::Migration[6.1]
   def change
     create_table :hokkaido_tag_relations do |t|
       t.references :hokkaido, null: false, foreign_key: true
-      t.references :tag, null: false, foreign_key: true
+      t.references :tag1, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20240412055326_drop_hokkaidotagrelations.rb
+++ db/migrate/20240412055326_drop_hokkaidotagrelations.rb
@@ -2,7 +2,7 @@ class DropHokkaidotagrelations < ActiveRecord::Migration[6.1]
     def change
       drop_table :hokkaido_tag_relations do |t|
         t.references :hokkaido, null: false, foreign_key: true
-        t.references :tag, null: false, foreign_key: true
+        t.references :tag1, null: false, foreign_key: true
         t.timestamps
       end
   end
